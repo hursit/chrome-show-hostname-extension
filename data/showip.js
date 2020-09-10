ce443a20b89b7c1372649fcd7ac56123f6b000ff
hursit <hursit.topal@enuygun.com>
2020-09-11 00:47:18 +0300

    .

--- data/showip.js
+++ data/showip.js
@@ -1,17 +1,43 @@
-window.onload = function() {
+window.onload = function () {
     fetchHostname();
 };
 
-
-document.getElementById("refresh-btn").addEventListener("click", fetchClientIP, false);
-document.getElementById("copy-btn").addEventListener("click", copyToClipboard, false);
-
-
 function fetchHostname() {
-    displayContent("1453");
+    chrome.cookies.get({url: 'https://www.enuygun.com', name: 'SERVERID-SH'},
+        function (cookie) {
+            if (cookie) {
+                displayHostname('hostname-sh', 'green', cookie.value);
+            } else {
+                displayHostname('hostname-sh', 'red', '');
+            }
+        });
+    chrome.cookies.get({url: 'https://www.enuygun.com', name: 'SERVERID-SAG'},
+        function (cookie) {
+            if (cookie) {
+                displayHostname('hostname-sag', 'green', cookie.value);
+            } else {
+                displayHostname('hostname-sag', 'red', '');
+            }
+        });
 }
 
-function displayContent(hostname) {
-    document.getElementById("ip-address").style.color = "green";
-    document.getElementById("ip-address").innerText = hostname;
+function displayHostname(id, color, text) {
+    document.getElementById(id).style.color = color;
+    document.getElementById(id).innerText = text;
+}
+
+
+document.getElementById("refresh-btn").addEventListener("click", clearCookies, false);
+
+function clearCookies() {
+    chrome.cookies.remove({"url": "https://www.enuygun.com", "name": "SERVERID-SH"});
+    chrome.cookies.remove({"url": "https://www.enuygun.com", "name": "SERVERID-SAG"});
+
+    window.close();
+
+    chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.reload(tab.id);
+    });
+
+    window.close();
 }
